69ee89233765d6d9e4dcabb5f8fc49ecb7b1fe0d
KARMIT\Karmit <karmit.dandona@gmail.com>
2019-02-10 00:04:20 -0800

    Updates to testing

--- testingScript.py
+++ testingScript.py
@@ -2,7 +2,8 @@ import requests
 import time
 import twilioAuth
 
-URL = "http://localhost:5000"
+# URL = "http://localhost:5000"
+URL = "https://calcium-scholar-231323.appspot.com"
 
 s = requests.Session()
 
@@ -10,17 +11,17 @@ r = s.post(URL + "/login", data={"name": "Caller"})
 
 cookies = r.cookies
 
-r = s.post(URL + "/reminders", data={"info": "Take medication", "time": time.time() + 20}, cookies=cookies)
+r = s.post(URL + "/reminders", data={"info": "Check on Grandma Louise", "time": time.time() + 20}, cookies=cookies)
 
 r = s.get(URL + "/reminders")
 
 data = r.json()["reminders"]
 
-reminderToUpdate = {}
-for reminderDict in data:
-    if reminderDict["info"] == "Take medication":
-        reminderToUpdate = reminderDict
-        break
+# reminderToUpdate = {}
+# for reminderDict in data:
+#     if reminderDict["info"] == "Take medication":
+#         reminderToUpdate = reminderDict
+#         break
 
-reminderToUpdate['callee'] = twilioAuth.participant1
-r = s.post(URL + "/update_reminder", data=reminderDict, cookies=cookies)
+# reminderToUpdate['callee'] = twilioAuth.participant1
+# r = s.post(URL + "/update_reminder", data=reminderDict, cookies=cookies)
