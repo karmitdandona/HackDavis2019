a101d9e018554765f5f0c0f562330afb3160cd7b
KARMIT\Karmit <karmit.dandona@gmail.com>
2019-02-10 05:38:28 -0800

    TestingScript no longer works (have to use postman now to bundle stuff as json). Updated main.py to use jsons appropriately.

--- testingScript.py
+++ testingScript.py
@@ -11,17 +11,19 @@ r = s.post(URL + "/login", data={"name": "Caller"})
 
 cookies = r.cookies
 
-r = s.post(URL + "/reminders", data={"info": "Check on Grandma Louise", "time": time.time() + 40}, cookies=cookies)
+r = s.post(URL + "/reminders", data={"info": "Take medication 4abc", "time": time.time() + 500}, cookies=cookies)
 
 r = s.get(URL + "/reminders")
 
-data = r.json()["reminders"]
+# data = r.json()["reminders"]
 
 # reminderToUpdate = {}
 # for reminderDict in data:
-#     if reminderDict["info"] == "Take medication":
+#     if reminderDict["info"] == "blah!":
 #         reminderToUpdate = reminderDict
 #         break
 
+# print(reminderToUpdate)
+
 # reminderToUpdate['callee'] = twilioAuth.participant1
-# r = s.post(URL + "/update_reminder", data=reminderDict, cookies=cookies)
+# r = s.post(URL + "/update_reminder", data=reminderToUpdate, cookies=cookies)
